29890aa881cee2153df674674ff3c720583a5630
Michael Normand <michael@mikes-dev-pc>
2020-01-23 16:10:23 -0500

    Adding build files

---
+++ app/build/precache-manifest.8c78e34008bba4e24f4efea9182595bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c28bde842a23ba4296ed7927a62fc84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e70c2ec7ed9111718a0",
+    "url": "/static/css/main.5f19a455.chunk.css"
+  },
+  {
+    "revision": "9b23a59de9208355810c",
+    "url": "/static/js/2.6d271777.chunk.js"
+  },
+  {
+    "revision": "4ebd4c934480c725a320d9506119b042",
+    "url": "/static/js/2.6d271777.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e70c2ec7ed9111718a0",
+    "url": "/static/js/main.676748da.chunk.js"
+  },
+  {
+    "revision": "3c1b4e08baea93986bd1",
+    "url": "/static/js/runtime-main.52eeffa5.js"
+  }
+]);
