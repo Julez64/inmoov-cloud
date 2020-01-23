1dd3334074312fd9960ca417caf3a0c928180dc5
Michael Normand <michael@mikes-dev-pc>
2020-01-23 16:07:46 -0500

    Changing servos indexes and angles

--- app/src/app.js
+++ app/src/app.js
@@ -14,13 +14,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Servo servo="0" angle="12" socket={this.state.socket} />
-        <Servo servo="1" angle="20" socket={this.state.socket} />
-        <Servo servo="2" angle="0" socket={this.state.socket} />
-        <Servo servo="3" angle="100" socket={this.state.socket} />
-        <Servo servo="4" angle="59" socket={this.state.socket} />
-        <Servo servo="5" angle="38" socket={this.state.socket} />
-        <Servo servo="6" angle="71" socket={this.state.socket} />
+        <Servo servo="0" angle="0" socket={this.state.socket} />
       </div>
     );
   }
