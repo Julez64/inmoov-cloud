60ba2f6c4c245caa11bd6b039133db2b2eeef76b
Michael Normand <michael@mikes-dev-pc>
2020-01-23 16:04:57 -0500

    Adding socket on servos

--- app/src/app.js
+++ app/src/app.js
@@ -3,17 +3,28 @@ import Servo from "./servo";
 import SocketIO from 'socket.io-client'
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      endpoint: 'http://localhost:8080',
+      servos: [],
+      socket: undefined
+    }
+  }
   render() {
     return (
       <div className="container">
-        <Servo servo="0" angle="12" />
-        <Servo servo="1" angle="20" />
-        <Servo servo="2" angle="0" />
-        <Servo servo="3" angle="100" />
-        <Servo servo="4" angle="59" />
-        <Servo servo="5" angle="38" />
-        <Servo servo="6" angle="71" />
+        <Servo servo="0" angle="12" socket={this.state.socket} />
+        <Servo servo="1" angle="20" socket={this.state.socket} />
+        <Servo servo="2" angle="0" socket={this.state.socket} />
+        <Servo servo="3" angle="100" socket={this.state.socket} />
+        <Servo servo="4" angle="59" socket={this.state.socket} />
+        <Servo servo="5" angle="38" socket={this.state.socket} />
+        <Servo servo="6" angle="71" socket={this.state.socket} />
       </div>
     );
+  }
+  componentDidMount() {
+    this.setState({socket: SocketIO(this.state.endpoint).connect()})
   }
 }
--- app/src/servo.js
+++ app/src/servo.js
@@ -8,9 +8,15 @@ class Servo extends React.Component {
             angle: this.props.angle
         }
         this.onAngleChange = this.onAngleChange.bind(this)
+        this.onMouseUp = this.onMouseUp.bind(this)
     }
     onAngleChange(event) {
         this.setState({angle: event.target.value})
+    }
+    onMouseUp() {
+        if (this.props.socket !== undefined) {
+            this.props.socket.emit('move', {id: this.props.servo, value: this.state.angle})
+        }
     }
     render() {
         return (
@@ -18,7 +24,7 @@ class Servo extends React.Component {
                 <h1>Servo #{this.props.servo}</h1>
                 <h3>Angle: {this.state.angle}</h3>
                 <br />
-                <input type='range' min='0' max='180' value={this.state.angle} onChange={this.onAngleChange} step='1'/>
+                <input type='range' min='0' max='180' value={this.state.angle} onChange={this.onAngleChange} step='1' onMouseUp={this.onMouseUp}/>
             </div>
         )
     }
