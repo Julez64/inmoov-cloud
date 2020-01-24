07e22e19add81d6e753747c30a7dd962a10b66c5
Beaky <alema027@hotmail.fr>
2020-01-24 15:43:18 -0500

    Python Spawn & get "/" cleaned up

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ let app = require('express')()
 let http = require('http').createServer(app)
 let io = require('socket.io')(http)
 let net = require('net')
-let spawn = require('child_process').spawn
-
-/*const pythonProcess = spawn('python3',["./main.py"])
-
-pythonProcess.stdout.on('data', (data) => {
-    console.log(data)
-})
-*/
-app.get("/", (req, res) => {
-	res.end("hello world")
-})
 
 io.on('connection', (socket) => {
 	let s = net.Socket({ allowHalfOpen: true })
