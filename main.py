6f13db469fa439ab38adbbc0e929c125c470abda
Entrybot64 <entrybotjs@gmail.com>
2020-01-24 15:11:03 -0500

    cleaned strings for python console

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ HOST = "127.0.0.1"
 PORT = 4000
 
 def moveServo(id, angle):
-	print(id)
-	print(angle)
+	print("Recieved command: Channel {ch} to angle {angle}".format(ch=id, angle=angle))
 	robot.servo[id].angle = angle
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -18,13 +17,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.listen()
         conn, addr = s.accept()
         with conn:
-            print('Connected by', addr)
+            print("Connected: {addr}".format(addr=addr))
             while True:
                 data = conn.recv(1024)
                 if not data:
                     break
 
-                data = str(data).split("'")[1].split("(")[1].split(")")[0].split(",")
+                data = str(data).split("(")[1].split(")")[0].split(",")
                 moveServo(int(data[0]),int(data[1]))
     except KeyboardInterrupt:
         s.close()
