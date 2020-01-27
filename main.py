95226d762d9aad078c5e9d50e2e0ba4d78964483
Nicolas Lemay <lemay.nicolas@carrefour.cegepvicto.ca>
2020-01-27 16:40:42 -0500

    Fixed main.py and created npm run dev command.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#from adafruit_servokit import ServoKit
+from adafruit_servokit import ServoKit
 import time
 import socket
 import json
 
-#robot = ServoKit(channels=16)
+robot = ServoKit(channels=16)
 
 HOST = "127.0.0.1"
 PORT = 4000
@@ -11,7 +11,7 @@ PORT = 4000
 
 def moveServo(id, angle):
     print("Recieved command: Channel {ch} to angle {angle}".format(ch=id, angle=angle))
-    #robot.servo[id].angle = angle
+    robot.servo[id].angle = angle
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -22,7 +22,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         with conn:
             print("Connected: {addr}".format(addr=addr))
             while True:
-                data = conn.recv(1024)
+                data = s.recv(1024)
                 print('Received stuff')
                 if not data:
                     break
