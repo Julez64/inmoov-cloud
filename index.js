3da19109beb919acd85e06a7cd9c37f54f5a4908
Beaky <alema027@hotmail.fr>
2020-01-27 16:43:37 -0500

    Prevent movement in case no sockets are connected

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ let http = require('http').createServer(app)
 let io = require('socket.io')(http)
 let net = require('net')
 
-
 let server = net.createServer((c) => {
-	
 	// 'connection' listener.
 	console.log('client connected ')
+	let connected = true
 	c.setMaxListeners(0)
 	c.on('end', () => {
+		connected = false
 		console.log('client disconnected')
 	})
 
 	io.on('connection', (socket) => {
-
 		socket.on('move', (data, err) => {
-			c.write(JSON.stringify((data)))
+			if (connected) {
+				c.write(JSON.stringify(data))
+			}
 		})
 
 		socket.on('disconnect', (data, end) => {
