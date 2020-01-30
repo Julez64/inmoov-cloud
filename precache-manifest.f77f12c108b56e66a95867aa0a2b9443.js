f3ff9bfb60c0118c7778370d7b827b40a14ecd22
Entrybot64 <entrybotjs@gmail.com>
2020-01-30 15:53:33 -0500

    Updates

---
+++ precache-manifest.f77f12c108b56e66a95867aa0a2b9443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d1d514599c442cbd561387dfdb2b2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dda41b3feb36c655bbb",
+    "url": "/static/css/main.39ef3c07.chunk.css"
+  },
+  {
+    "revision": "0ffe9b82118665f2c2cb",
+    "url": "/static/js/2.9dc61bfa.chunk.js"
+  },
+  {
+    "revision": "4ebd4c934480c725a320d9506119b042",
+    "url": "/static/js/2.9dc61bfa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4dda41b3feb36c655bbb",
+    "url": "/static/js/main.399fe460.chunk.js"
+  },
+  {
+    "revision": "3c1b4e08baea93986bd1",
+    "url": "/static/js/runtime-main.52eeffa5.js"
+  }
+]);
